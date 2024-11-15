c836ac7f65d76ea2966be412506644e641154429
Aisha2mohammed <hayunadus111@gmail.com>
2024-11-15 23:55:28 +0300

    Add how to get the string and shifting number

--- ceasar_cipher/cipher.rb
+++ ceasar_cipher/cipher.rb
@@ -1,12 +1,12 @@
 alphabet = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
-value = gets.to_i.chomb
+value = gets.chomb
+shiftNumber = gets.to_i.chomb
+
+cipher(value,shiftNum)
+
+def cipher(string,shift = 0) 
 
 
-cipher(value)
-
-def cipher(shift = 0) 
 
 
-
-  
 end
