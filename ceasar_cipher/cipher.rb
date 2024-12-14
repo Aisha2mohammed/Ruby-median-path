671f51eba2dd629bd6cdd2b071224a72dae909e6
Aisha2mohammed <hayunadus111@gmail.com>
2024-12-14 22:27:09 +0300

    The basic logic

--- ceasar_cipher/cipher.rb
+++ ceasar_cipher/cipher.rb
@@ -13,7 +13,8 @@ def cipher(string,shift = 0,alphabet)
     if alphabet[char.to_sym]
       new_index = (alphabet[char.to_sym] + shift - 1) % 26 + 1
       alphabet.key(new_index)
-    
+    else
+      char
     end
-
+  end
 end
